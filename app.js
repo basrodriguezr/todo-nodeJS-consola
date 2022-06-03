f4c19dbc4f7c06b3a274636d1cc88e5ca35b4731
bastian rodríguez <basrodriguezr@gmail.com>
2022-06-03 00:08:01 -0400

    Logica para el manejo de tareas

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 require('colors');
 //const {mostrarMenu, pausa} = require('./helpers/mensajes');
 const {inquireMenu,pausa} = require('./helpers/inquirer');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
+
 
 console.clear();
 
 const main = async() =>{
     let opt='';
-
+    const tareas = new Tareas();
+    
     while(opt!='0'){
         //opcion =  await mostrarMenu();
         opt =  await inquireMenu();
-        
+        const tarea = new Tarea('Estudiar NODE');
+       
+
+        tareas.listado[tarea.id] = tarea;
         await pausa();        
     }
 
